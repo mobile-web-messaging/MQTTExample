386f67bd5f992299c91d65087d5bdc6c6724bb48
Jeff Mesnil <jmesnil@gmail.com>
2014-02-15 16:44:58 +0100

    update example

--- MQTTExample/MQTTViewController.m
+++ MQTTExample/MQTTViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "MQTTViewController.h"
+#import <MQTTKit.h>
+
+#define kMQTTServerHost @"iot.eclipse.org"
+#define kTopic @"MQTTExample/LED"
 
 @interface MQTTViewController ()
+
+@property (weak, nonatomic) IBOutlet UISwitch *subscribedSwitch;
+
+@property (nonatomic, strong) MQTTClient *client;
 
 @end
 
@@ -17,13 +25,55 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    NSString *clientID = [UIDevice currentDevice].identifierForVendor.UUIDString;
+    self.client = [[MQTTClient alloc] initWithClientId:clientID];
+
+    UISwitch *subSwitch = self.subscribedSwitch;
+
+    // define the message handler that will handle the received MQTT messages
+    [self.client setMessageHandler:^(MQTTMessage *message) {
+        BOOL on = [message.payloadString boolValue];
+        // the MQTTClientDelegate methods are called from a GCD queue.
+        // Any update to the UI must be done on the main queue
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [subSwitch setOn:on animated:YES];
+        });
+    }];
+    // connect the MQTT client
+    [self.client connectToHost:kMQTTServerHost completionHandler:^(NSUInteger code) {
+        NSLog(@"client is connected with id %@", clientID);
+        // once the client is connected, subscribe to the topic
+        [self.client subscribe:kTopic
+         withCompletionHandler:^(NSArray *grantedQos) {
+             NSLog(@"subscribed to topic %@", kTopic);
+         }];
+    }];
 }
 
+- (void)dealloc
+{
+    [self.client disconnectWithCompletionHandler:^(NSUInteger code) {
+        NSLog(@"MQTT is disconnected");
+    }];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - IBActions
+
+- (IBAction)switchUpdated:(id)sender {
+    BOOL on = [sender isOn];
+    NSString *payload = [NSNumber numberWithBool:on].stringValue;
+
+    [self.client publishString:payload
+                       toTopic:kTopic
+                       withQos:AtMostOnce
+                        retain:YES
+             completionHandler:nil];
 }
 
 @end
